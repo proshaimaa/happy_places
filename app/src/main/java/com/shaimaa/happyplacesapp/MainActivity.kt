cf1f77f52f3765b7acf71bf08fd6c1cce3e75b6b
Shaimaa Elshafei <shaimaa@Shaimaas-MacBook-Pro.local>
2020-12-13 17:30:17 +0300

    added files

---
+++ app/src/main/java/com/shaimaa/happyplacesapp/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.shaimaa.happyplacesapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var x = 5
+    }
+}
