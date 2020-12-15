f64094940c326600952ccb796d2dcac5db39e7db
Shaimaa Elshafei <shaimaa@Shaimaas-MacBook-Pro.local>
2020-12-15 13:13:31 +0300

    change toolbartheme color and make add happy place layout

--- app/src/main/java/com/shaimaa/happyplacesapp/MainActivity.kt
+++ app/src/main/java/com/shaimaa/happyplacesapp/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.shaimaa.happyplacesapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var x = 5
+        fabAddHappyPlace.setOnClickListener {
+            val intent = Intent(this,AddHappyPlaceActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
