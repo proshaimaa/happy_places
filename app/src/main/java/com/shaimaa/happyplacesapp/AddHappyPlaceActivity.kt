7029ca98d8374eb97147f65419d1939bcaa0fe76
Shaimaa Elshafei <shaimaa@Shaimaas-MacBook-Pro.local>
2020-12-15 14:29:41 +0300

    dexter permission

--- app/src/main/java/com/shaimaa/happyplacesapp/AddHappyPlaceActivity.kt
+++ app/src/main/java/com/shaimaa/happyplacesapp/AddHappyPlaceActivity.kt
@@ -1,10 +1,18 @@
 package com.shaimaa.happyplacesapp
 
+import android.Manifest
+import android.app.AlertDialog
 import android.app.DatePickerDialog
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 import kotlinx.android.synthetic.main.activity_add_happy_place.*
 import java.text.SimpleDateFormat
 import java.util.*
@@ -34,6 +42,7 @@ class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener{
         }
 
         et_date.setOnClickListener(this)
+        tv_add_image.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
@@ -44,7 +53,40 @@ class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener{
                 cal.get(Calendar.MONTH),
                 cal.get(Calendar.DAY_OF_MONTH)).show()
             }
+            R.id.tv_add_image -> {
+                val pictureDialog = AlertDialog.Builder(this)
+                pictureDialog.setTitle("Select Action")
+                val pictureDialogItem = arrayOf("Select photo from Gallery","Capture photo from Camera")
+                pictureDialog.setItems(pictureDialogItem){
+                    dialog, which ->
+                    when(which){
+                        0 -> choosePhotoFromGallery()
+                        1 -> Toast.makeText(this,"Camera selection coming soon ...",Toast.LENGTH_LONG).show()
+                    }
+                }
+                pictureDialog.show()
+            }
         }
+    }
+
+    private fun choosePhotoFromGallery() {
+
+        Dexter.withContext(this).withPermissions(
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+        ).withListener(object: MultiplePermissionsListener{
+            override fun onPermissionsChecked(p0: MultiplePermissionsReport?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onPermissionRationaleShouldBeShown(
+                p0: MutableList<PermissionRequest>?,
+                p1: PermissionToken?
+            ) {
+                TODO("Not yet implemented")
+            }
+
+        }).check()
     }
 
     private fun updateDateInView(){
